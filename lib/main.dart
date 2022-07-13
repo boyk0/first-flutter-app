276cc145a251609ef904ace1abdefbb48ec4ed84
boyk0 <v_boiko-itssv@kw.com>
2022-07-13 12:25:06 +0300

    add title and text

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,14 @@ void main () => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first Title'),
+        ),
+        body: Text('My text'),
+      ),
+    );
 
   }
 
