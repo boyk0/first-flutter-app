907588f5b3e49995cd4172e48dae070236fa8c6b
boyk0 <v_boiko-itssv@kw.com>
2022-07-14 12:59:24 +0300

    format var to const

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
 
-    var questions = [
+    const questions = const [
       {
         'questionText': 'What\'s your favorite color ?',
         'answers': ['red', 'black', 'white', 'yellow'],
