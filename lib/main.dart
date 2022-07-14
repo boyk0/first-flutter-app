09ca7d5986fff4a9ca43363baf94f65c681561f6
boyk0 <v_boiko-itssv@kw.com>
2022-07-14 13:37:48 +0300

    add check for finishing quiz

--- lib/main.dart
+++ lib/main.dart
@@ -15,35 +15,38 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   int _questionIndex = 0;
 
+  final questions = const [
+    {
+      'questionText': 'What\'s your favorite color ?',
+      'answers': ['red', 'black', 'white', 'yellow'],
+    },
+    {
+      'questionText': 'What\'s your favorite animal ?',
+      'answers': ['cat', 'dog', 'fish'],
+    },
+    {
+      'questionText': 'What\'s your favorite food ?',
+      'answers': ['pizza', 'pizza', 'pizza', 'pizza'],
+    },
+  ];
+
   void _answerQuestion() {
-    setState(() => _questionIndex++);
-    print('answear chosen index: ${_questionIndex}');
+    if(_questionIndex < questions.length) {
+      setState(() => _questionIndex++);
+    }
+    print('answer chosen index: ${_questionIndex}');
   }
 
   @override
   Widget build(BuildContext context) {
 
-    const questions = const [
-      {
-        'questionText': 'What\'s your favorite color ?',
-        'answers': ['red', 'black', 'white', 'yellow'],
-      },
-      {
-        'questionText': 'What\'s your favorite animal ?',
-        'answers': ['cat', 'dog', 'fish'],
-      },
-      {
-        'questionText': 'What\'s your favorite food ?',
-        'answers': ['pizza', 'pizza', 'pizza', 'pizza'],
-      },
-    ];
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My first Title'),
         ),
-        body: Column(
+        body: _questionIndex < questions.length ? Column(
           children: [
             Question(questions[_questionIndex]['questionText']),
             ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
@@ -51,6 +54,8 @@ class _MyAppState extends State<MyApp> {
             }).toList(),
             Text('questionIndex: ${_questionIndex}'),
           ],
+        ) : Center(
+            child: Text('You did it'),
         ),
       ),
     );
